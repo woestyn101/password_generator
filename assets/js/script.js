d18371fdc4d5e2ff10c2809aab9575176d1d5f01
Marius Du Plooy <woestyn101@gmail.com>
2023-11-20 23:25:56 -0500

    min and max characters conditio fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,59 +28,104 @@ var newPasswordArray = [];
 
 
 document.getElementById("generate").onclick = function () {
+
+  
 // prompt variables
   var amountCharacters = prompt("How many characters do you want?");
-  var lowerCaseLetters = confirm("Do you want lowercase letters?");
-  var upperCaseLetters = confirm("Do you want uppercase letters?");
-  var numbers = confirm("Do you want numbers?");
-  var specialCharacters = confirm("Do you want to include special characters?");
-  if (lowerCaseLetters == true){
-    passwordArray = arrayLetters.sort(() => 0.5 - Math.random()); // 
-    //console.log(passwordArray);
-  }else {
-    passwordArray = [];
+  
+  
+  while (amountCharacters < 8){
+    amountCharacters = prompt("Choose at least 8 characters!");
   }
-  if(upperCaseLetters == true)
-  {
-    const upperCaseShuffled = arrayLetters.sort(() => 0.5 - Math.random()); 
-    const upperCaseArray = upperCaseShuffled.map(item => item.toUpperCase());
-    passwordArray = passwordArray.concat(upperCaseArray);
-    passwordArray = passwordArray.sort(() => 0.5 - Math.random()); 
-    console.log(passwordArray);
-  }else {
-   passwordArray = passwordArray.concat(passwordArray);
+
+  while (amountCharacters > 128){
+    amountCharacters = prompt("Choose a maximum of 128 characters!");
   }
-  if (numbers == true){
-    const numbersShuffled = arrayNumbers.sort(() => 0.5 - Math.random());
-    
-    passwordArray = passwordArray.concat(numbersShuffled);
-    passwordArray = passwordArray.sort(() => 0.5 - Math.random());
-  
-    console.log(passwordArray);
-  
-  } else {
-    passwordArray = passwordArray.concat(passwordArray);
-  }
-  
-  if (specialCharacters == true){
-    const specialShuffled = arraySymbols.sort(() => 0.5 - Math.random());
-    
-    passwordArray = passwordArray.concat(specialShuffled);
-    passwordArray = passwordArray.sort(() => 0.5 - Math.random());
-  
-    console.log(passwordArray);
-  
-  }else {
-    passwordArray = passwordArray.concat(passwordArray);
-  }
-  
-  passwordArray = passwordArray.slice(0,amountCharacters)
-  
-  passwordArray = passwordArray.join('');
-  
-  document.getElementById("answer").innerHTML = passwordArray;
-  document.getElementById("password1").innerHTML = passwordArray;
+
   
   
 
-}
+    var lowerCaseLetters = confirm("Do you want lowercase letters?");
+  var upperCaseLetters = confirm("Do you want uppercase letters?");
+  var numbers = confirm("Do you want numbers?");
+  var specialCharacters = confirm("Do you want to include special characters?");
+
+    if (lowerCaseLetters == true){
+      passwordArray = arrayLetters.sort(() => 0.5 - Math.random()); // 
+     
+    }else {
+      passwordArray = [];
+    }
+    if(upperCaseLetters == true)
+    {
+      const upperCaseShuffled = arrayLetters.sort(() => 0.5 - Math.random()); 
+      const upperCaseArray = upperCaseShuffled.map(item => item.toUpperCase());
+      passwordArray = passwordArray.concat(upperCaseArray);
+      passwordArray = passwordArray.sort(() => 0.5 - Math.random()); 
+      console.log(passwordArray);
+    }else {
+     passwordArray = passwordArray.concat(passwordArray);
+    }
+    if (numbers == true){
+      const numbersShuffled = arrayNumbers.sort(() => 0.5 - Math.random());
+      
+      passwordArray = passwordArray.concat(numbersShuffled);
+      passwordArray = passwordArray.sort(() => 0.5 - Math.random());
+    
+      console.log(passwordArray);
+    
+    } else {
+      passwordArray = passwordArray.concat(passwordArray);
+    }
+    
+    if (specialCharacters == true){
+      const specialShuffled = arraySymbols.sort(() => 0.5 - Math.random());
+      
+      passwordArray = passwordArray.concat(specialShuffled);
+      passwordArray = passwordArray.sort(() => 0.5 - Math.random());
+    
+      console.log(passwordArray);
+    
+    }else {
+      passwordArray = passwordArray.concat(passwordArray);
+    }
+    
+    passwordArray = passwordArray.slice(0,amountCharacters)
+    
+    passwordArray = passwordArray.join('');
+    
+    
+    document.getElementById("password1").innerHTML = passwordArray;
+  
+    
+  
+    
+
+  }
+
+    
+
+
+
+
+
+
+
+ 
+
+
+  
+  
+
+
+
+// code resources
+
+// https://dirask.com/posts/JavaScript-get-multiple-random-elements-from-array-D9WM0j
+
+
+// https://www.w3schools.com/jsref/jsref_concat_array.asp
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
